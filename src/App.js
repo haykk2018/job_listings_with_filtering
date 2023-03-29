1d51c420168d3b8f94cd898f497fb8a7ed96cc7b
hayk <haykk2018@gmail.com>
2023-03-29 13:05:07 +0400

    START PROJECT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {TheHeader} from "./components/TheHeader";
+import {FilterPanel} from "./components/FilterPanel";
+import {JobList} from "./components/JobList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <>
+        <TheHeader/>
+        <div className='container'>
+            <FilterPanel/>
+            <JobList/>
+        </div>
+    </>
 }
 
 export default App;
---
+++ src/components/FilterPanel.jsx
@@ -0,0 +1,22 @@
+import {Stack} from "../UI/Stack";
+import {Card} from "../UI/Card";
+import {Badge} from "../UI/Badge";
+
+
+const FilterPanel = () => {
+  return (
+    <Card className="filter-panel">
+      <div className="filter-panel-wrapper">
+        <Stack>
+          <Badge variant="clearable">Frontend</Badge>
+          <Badge variant="clearable">Backend</Badge>
+          <Badge variant="clearable">React</Badge>
+        </Stack>
+
+        <button className='link'>Clear</button>
+      </div>
+    </Card>
+  )
+}
+
+export {FilterPanel};
