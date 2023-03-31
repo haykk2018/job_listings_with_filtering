449ba802383cffe270034f255b2779d4762b37b4
hayk <haykk2018@gmail.com>
2023-03-31 12:01:14 +0400

    almost completed project

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,8 +1,9 @@
 import {JobPosition} from './JobPosition';
 import {useDispatch, useSelector} from "react-redux";
-import {selectAllPositions} from "../store/positions/position-selectors";
+import {selectVisiblePositions} from "../store/positions/position-selectors";
 
 import {addFilter} from "../store/filters/filter-actions";
+import {selectFilters} from "../store/filters/filter-selectors";
 
 const JobList = () => {
 
@@ -12,7 +13,8 @@ const JobList = () => {
         dispatch(addFilter(filter))
     }
 
-    const positions = useSelector(selectAllPositions);
+    const currentFilters = useSelector(selectFilters);
+    const positions = useSelector(state => selectVisiblePositions(state, currentFilters));
 
     return (
         <div className='job-list'>
